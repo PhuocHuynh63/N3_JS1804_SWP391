2e88af1db09b6e4e369189309c4200b48ab58761
Huỳnh Minh Phước <118465492+PhuocHuynh63@users.noreply.github.com>
2024-06-19 13:34:50 +0700

    Phuoc2 (#15)
    * Update image in subcategory, add attribute images subcate
    
    * Update image in subcategory, add attribute images subcate
    
    * Add product

--- MeBeTest.sql
+++ MeBeTest.sql
@@ -167,9 +167,9 @@ CREATE TABLE product (
     FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
 );
 
+
 INSERT INTO product (subcategory_id, slug, name, images, [description], price, sale_price, [status], total_sold, product_view)
 VALUES
-   
     (1, N'san-pham-dinh-duong-cong-thuc-chuan-so-3-withmom-cesar', N'Sản phẩm dinh dưỡng công thức Withmom Cesar số 3', N'sua-withmom-cesar-so-3.webp', N'Sữa tươi loại 1-A không kháng sinh 17,54% (Hàn Quốc, theo tiêu chuẩn số lượng vi khuẩn), lactose, dextrin, dầu ăn hỗn hợp [dầu đậu nành, dầu dừa, dầu chế biến 6,02% (dầu cọ, dầu hướng dương)', 799.000, 700.000, N'Còn hàng', 0, 0),
     (1, N'sua-aptamil-duc-so-3', N'Sữa Aptamil Đức số 3', N'sua-Aptamil-duc.jpg', N'Lấy đầy muỗng sữa và gạt ngang miệng (1 muỗng Aptamil 3 gạt ngang tương đương 5g sữa) đong 4 muỗng sữa pha với 120ml nước được dung dịch 130ml sữa.', 595.000, 0, N'Còn hàng', 0, 0),
     (1, N'sua-bot-aptamil-essensis-organic-so-1', N'Sữa Aptamil Đức số 3', N'sua-Aptamil-Essensis-Organic-1.jpg', N'Sữa Aptamil Essensis Organic số 1 - dòng sữa công thức cao cấp dành cho trẻ', 1295.000, 0, N'Còn hàng', 0, 0),
@@ -179,7 +179,35 @@ VALUES
     (2, N'sua-nuoc-fruto-vi-mam-xoi-6m', N'Sữa nước Fruto vị mâm xôi 6M', N'sua-nuoc-Fruto-mam-xoi-1.jpg', N'Sữa bột nguyên kem, mâm xôi nghiền, bột gạo, đường fructose, maltodextrin, prebiotic-inulin, nước.', 34.900, 29.900, N'Còn hàng', 0, 0),
 
 	(3, N'sbps-nuvi-grow-110ml-1y', N'SBPS Nuvi Grow 110ml 1Y+','/images/sbps_nuvi_grow_180ml.webp', N'DINH DƯỠNG TỐI ƯU CHIỀU CAO', 32000.00, 0, N'Còn hàng', 0, 0),
-    (3, N'sua-nuvi-grow-iq-step-3-900g', N'Sữa Nuvi Grow IQ Step 3 900g', N'nuvi_grow_iq_step3.jpg', N'Sữa bột Nuvi Grow IQ Step 3 với công thức IQ Advance', 385000.00, 370000.00, N'Còn hàng', 0, 0)
+    (3, N'sua-nuvi-grow-iq-step-3-900g', N'Sữa Nuvi Grow IQ Step 3 900g', N'nuvi_grow_iq_step3.jpg', N'Sữa bột Nuvi Grow IQ Step 3 với công thức IQ Advance', 385000.00, 370000.00, N'Còn hàng', 0, 0),
+
+	(6, N'bim-quan-moony-natural', N'Bỉm quần Moony Natural', N'ta_quan_moony_natural.jpg', N'Moony Natural an toàn cho làn da sơ sinh! Lần đầu tiên xuất hiện tại Nhật Bản, Moony Natural là dòng tã siêu cao cấp, với sự kết hợp của mặt bông hữu cơ và 5 thiết kế nổi bật khiến sản phẩm êm mềm lành tính cho làn da sơ sinh của bé.', 470.000, 420.000,N'Còn hàng', 0, 0),
+    (6, N'bim-quan-bobby', N'Bỉm quần Bobby', N'bim-quan-bobby.jpg', N'Tã quần Bobby đến từ thương hiệu Unicharm Nhật Bản với những đặc điểm nổi trội như bề mặt tã 3D thông minh, mặt đáy siêu thấm và siêu thoát ẩm, lưng chun co giãn linh hoạt chống tràn hiệu quả,... sẽ mang đến cho bé một sản phẩm chăm sóc sức khỏe tuyệt vời trong những năm tháng đầu đời. Mẹ yên tâm rằng bé sẽ vô cùng thoải mái, dễ chịu khi mặc tã. Bé nhà bạn sẽ tự do vận động, vui chơi khám phá thế giới xung quanh mà không bị làm phiền. Sản phẩm với bịch lớn 48 miếng mang đến sự tiện lợi khi sử dụng.', 199.000, 0 ,N'Còn hàng', 0, 0),
+	(6, N'bim-quan-huggies', N'Bỉm quần Huggies', N'bim-quan-huggies,jpg', N'Đệm mây 4 chiều là cải tiến mới của tã quần Huggies Dry, mang lại cho bé sự dễ chịu, tạm biệt vết hằn đỏ trên da do mặc tã. Mẹ có thể thoải mái cho bé mặc tã quần cả ngày không còn lo bé bị khó chịu, hằn đỏ lên da.', 192.000, 190.000, N'Còn hàng', 0, 0),
+
+	(7, N'caryn-ta-giay-nguoi-lon-size-xl10-mieng', N'CARYN-TÃ GIẤY NGƯỜI LỚN SIZE XL10 miếng', N'caryn-ta-giay-nguoi-lon-size-xl10-mieng.jpg', N'Dành cho người hạn chế khả năng đi lại', 125.000, 0, N'Còn hàng', 0, 0),
+	(7, N'ta-dan-caryn-m-l40', N'Tã dán Caryn M-L40', N'ta-dan-caryn-m-l40.jpg', N'	Dành cho người hạn chế khả năng đi lại',395.000, 0, N'Còn hàng', 0, 0),
+	(7, N'ta-giay-caryn-m-l-3-mieng', N'Tã giấy Caryn M/L 3 miếng', N'ta-giay-caryn-m-l-3-mieng.jpg', N'Màng đáy Air-Active Tã dán Caryn có màng đáy Air-Active với tối đa diện tích biết thở dạng vải, mềm mại và tránh hằm bí, giúp da hô hấp với không khí tự nhiên.',34.000, 30.000, N'Còn hàng', 0, 0),
+
+	(8, N'be-bon-cau-hong-chicco', N'Bệ bồn cầu Chicco màu hồng', N'be-bon-cau-hong-chicco.jpg', N'Đang cập nhật nội dung',159.000 , 0, N'Còn hàng', 0, 0),
+	(8, N'bo-phi-thuyen-holla-hong', N'Bô phi thuyền Holla hồng', N'bo-phi-thuyen-holla-hong.jpg', N'Thiết kế thoải mái, phù hợp với chiều cao của trẻ. Phần bô đựng bên trong có thể tháo rời và dễ dàng vệ sinh.', 350.000, 300.000, N'Còn hàng', 0, 0),
+	(8, N'bo-hinh-khi-mau-xanh-royalcare', N'Bô hình khỉ màu xanh Royalcare', N'bo-hinh-khi-mau-xanh-royalcare.jpg', N'Đang cập nhật nội dung', 239.000, 234.900, N'Còn hàng', 0, 0),
+
+	(9, N'ban-chai-chu-u-hoat-hinh-3d-ht622-cho-be', N'Bàn chải chữ U hoạt hình 3D HT622 cho bé', N'ban-chai-chu-u-hoat-hinh-3d-ht622-cho-be.jpg', N'Đang cập nhật thông tin', 39.000, 0, N'Còn hàng', 0, 0),
+	(9, N'gac-ro-luoi-dr-papie-30pcs', N'Gạc răng miệng Dr.PaPie rơ lưỡi cho bé từ sơ sinh', N'gac-ro-luoi-dr-papie-30pcs.jpg', N'Đang cập nhật thông tin', 110.000, 0, N'Còn hàng', 0, 0),
+	(9, N'gel-danh-rang-tre-em-eq-tech-junior-75g', N'Gel đánh răng trẻ em EQ Tech Junior 75g', N'gel-danh-rang-tre-em-eq-tech-junior-75g.jpg', N'Đang cập nhật thông tin', 29.000, 0, N'Còn hàng', 0, 0),
+
+	(10, N'dau-goi-ngan-rung-toc-dersante-150ml', N'Dầu gội ngăn rụng tóc Dersante 150ml', N'dau-goi-ngan-rung-toc-dersante-150ml.jpg', N'Dầu gội ngăn rụng tóc Dersante giúp: làm sạch da đầu và tóc, làm sạch gàu, ngăn ngừa và cải thiện tình trạng tóc: xơ, rối, chẻ ngọn, gãy, rụng,… cho mái tóc trông dày, bóng mượt và chắc khỏe hơn. Góp phần thúc đẩy nhanh quá trình phục hồi tóc bị hư tổn và kích thích sự phát triển của nang tóc.', 145.000, 145.000, N'Còn hàng', 0, 0),
+	(10, N'sua-tam-goi-toan-bebble-shampoo-body-wash-400ml', N'Sữa tắm gội toàn Bebble Shampoo & Body Wash 400ml', N'sua-tam-goi-toan-bebble-shampoo-body-wash-400ml.jpg', N':Sữa tắm gội nhẹ nhàng chăm sóc, làm sạch, bảo vệ da khỏi tác động từ môi trường bên ngoài, cung cấp độ ẩm cho da mềm mại, hỗ trợ điều trị khô da, viêm da cơ địa, vảy nến.', 392.000, 389.000, N'Còn hàng', 0, 0),
+	(10, N'dau-goi-bac-ha-mat-ruoi-h-s-1200', N'Dầu Gội Bạc Hà Mát Rượi H&S 1200', N'dau-goi-bac-ha-mat-ruoi-h-s-1200.jpg', N'Đang cập nhật nội dung', 328.000, 0, N'Còn hàng', 0, 0),
+
+	(11, N'sua-bot-aptamil-nz-so-2-900gr', N'Sữa bột Aptamil NZ số 2 (900gr) dành cho bé 1-2 tuổi', N'sua-bot-aptamil-nz-so-2-900gr.jpg', N'Sữa Aptamil NewZealand số 2 hỗ trợ bổ sung lợi khuẩn đường ruột ở trẻ bú sữa công thức tương tự trẻ bú sữa mẹ, tăng cường miễn dịch giúp trẻ bắt kịp đà tăng trưởng. Sữa Aptamil NewZealand số 2 được các chuyên gia khuyên dùng cho trẻ nhỏ. Việc hình thành thói quen ăn uống góp phần tạo nên thói quen dinh dưỡng. Đối với trẻ giai đoạn 12 – 24 tháng tuổi thì bên cạnh chế độ ăn dặm, sữa được xem là một phần quan trọng vì chứa nhiều dưỡng chất quan trọng cho sự phát triển toàn diện.Sữa còn cung cấp lượng đạm, chất béo, một số vitamin, khoáng chất và năng lượng giúp trẻ phát triển. Hệ vi sinh đường ruột cũng khác biệt rất nhiều ở trẻ bú sữa mẹ và sữa công thức.', 619.000, 0, N'Còn hàng', 0, 0),
+	(11, N'sua-bot-aptakid-nz-so-3-900gr-24m-3', N'Sữa bột Aptakid NZ số 3 (900gr) dành cho bé trên 2 tuổi', N'sua-bot-aptakid-nz-so-3-900gr-24m-3.jpg', N'Một sản phẩm sữa tốt cho trẻ sơ sinh và trẻ nhỏ phải được hướng đến các lợi ích có trong sữa mẹ. Mà sữa mẹ thì diệu kỳ đến mức cực kỳ phức tạp, nhưng khi khoa học phát triển thì mọi bí mật đều sẽ được tìm ra. Và SYNBIOTIC là sự kết hợp của Prebiotics và Probiotics được tìm thấy rất nhiều trong sữa mẹ, đây là phát minh khoa học mới nhất của thương hiệu DANONE đưa vào sản phẩm Aptakid.', 619.000, 0, N'Còn hàng', 0, 0),
+	(11, N'sua-bau-danmilko-mamature-800g', N'Sữa bầu Danmilko Mamature 800g', N'sua-bau-danmilko-mamature-800g.jpg', N'Với hương vị thanh nhẹ từ Châu Âu, sữa bầu Danmilko Mamature cung cấp các dưỡng chất thiết yếu như DHA, Choline, Axit Folic giúp hỗ trợ thai kỳ và bổ sung và bù đắp dinh dưỡng cho mẹ trong suốt thai kỳ.', 495.000, 490.000, N'Còn hàng', 0, 0),
+
+	(13, N'lot-chong-tham-chong-trao-nguoc-hybaby', N'Lót chống thấm chống trào ngược Hybaby', N'lot-chong-tham-chong-trao-nguoc-hybaby.jpg', N'Lót thay tã, lót ngủ Hybaby cho bé với mẫu mã xinh xắn, thấm hút mồ hôi, chất lỏng ngăn thấm ngược sẽ là lựa chọn số 1 cùng mẹ chăm sóc bé yêu.', 169.000, 0, N'Còn hàng', 0, 0),
+	(13, N'mieng-lot-tham-sua-ku-ku-36-mieng', N'Miếng lót thấm sữa Ku-Ku 36 miếng', N'mieng-lot-tham-sua-ku-ku-36-mieng.jpg', N'Miếng Lót Thấm Sữa Kuku là loại sử dụng một lần, tiện mang theo để sử dụng hằng ngày. Sản phẩm giúp bạn chăm sóc vệ sinh khuôn ngực, tránh cho bầu vú không bị hăm vì ướt sữa trong giai đoạn cho con bú.', 115.000, 100.000, N'Còn hàng', 0, 0),
+	(13, N'lot-tham-sua-moby-60-mieng', N'Lót thấm sữa Moby ( 60 miếng )', N'lot-tham-sua-moby-60-mieng.jpg', N'Thiết kế đường viền khóa chống tràn 4 bên ngăn rò gỉ hiệu quả, Miếng lót thấm sữa Baby Moby 60 miếng với lớp gel siêu thấm, nhanh chóng thấm hút lượng sữa thừa, dàn đều và khóa chặt chúng bên trong, không gây tràn ngược trở lại. Bề mặt đường lưới kim cương tăng cường độ mềm mại và sự thoải mái bởi không khí sẽ dễ dàng lưu thông bên trong tạo sự thông thoáng, giữ cho ngực mẹ luôn khô ráo và thoáng mát suốt ngày dài.', 220.000, 0, N'Còn hàng', 0, 0)
 -- Tạo bảnh Inventory
 CREATE TABLE inventory (
 	inventory_id INT PRIMARY KEY IDENTITY(1,1),
