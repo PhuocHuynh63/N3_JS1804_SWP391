88910bb0eb558c67cf9d3fdff164b579f900fe29
PhuocHuynh63 <phuochmse171830@fpt.edu.vn>
2024-07-05 01:35:12 +0700

    Fix folder Admin, data Dashboard, List Category, Subcategory, User

---
+++ FE/FE_MeBeReact/app-project-master/src/pages/adminPage/userPage/PopupAddUser.js
@@ -0,0 +1,161 @@
+import React, { useState } from 'react';
+import './PopupAddUser.css';
+import { Modal } from 'antd';
+
+const PopupAddUser = ({ show, handleClose }) => {
+
+    /**
+     * Form data
+     */
+    const [formData, setFormData] = useState({
+        userName: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+        password: '',
+        status: '',
+        birthOfDate: '',
+        role: '',
+    });
+    //-----End-----//
+
+
+    /**
+     * Handle change input
+     * @param {*} e 
+     */
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value,
+        });
+    };
+    //-----End-----//
+
+
+    /**
+     * Handle form submission
+     * @param {*} e 
+     */
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({
+            userName: formData.userName,
+            firstName: formData.firstName,
+            lastName: formData.lastName,
+            email: formData.email,
+            phoneNumber: formData.phoneNumber,
+            password: formData.password,
+            status: formData.status,
+            role: formData.role,
+        });
+    };
+    //-----End-----//
+
+
+    return (
+        <Modal visible={show} onCancel={handleClose} footer={null} width={"auto"} centered>
+            <div className="admin-user-add">
+                <h1>Thêm người dùng mới</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group username">
+                        <label htmlFor="userName">Tên đăng nhập</label>
+                        <input
+                            type="text"
+                            id="userName"
+                            value={formData.userName}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group firstname">
+                        <label htmlFor="firstName">Họ</label>
+                        <input
+                            type="text"
+                            id="firstName"
+                            value={formData.firstName}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group lastname">
+                        <label htmlFor="lastName">Tên</label>
+                        <input
+                            type="text"
+                            id="lastName"
+                            value={formData.lastName}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group email">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group email">
+                        <label htmlFor="phoneNumber">Số điện thoại</label>
+                        <input
+                            type="phoneNumber"
+                            id="phoneNumber"
+                            value={formData.phoneNumber}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group password">
+                        <label htmlFor="password">Mật khẩu</label>
+                        <input
+                            type="password"
+                            id="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group role">
+                        <label htmlFor="role">Ủy quyền</label>
+                        <select
+                            id="role"
+                            value={formData.role}
+                            onChange={handleChange}
+                            required
+                        >
+                            <option value="">--- Chọn quyền ---</option>
+                            <option value="member">Thành viên</option>
+                            <option value="staff">Nhân viên</option>
+                            <option value="admin">Quản trị viên</option>
+                        </select>
+                    </div>
+
+                    <div className="form-group hidden">
+                        <input
+                            id="status"
+                            value={1}
+                            onChange={handleChange}
+                            type="hidden"
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <button className="btn-add" type="submit">Thêm người dùng</button>
+                    </div>
+                </form>
+            </div>
+        </Modal>
+    );
+};
+
+export default PopupAddUser;
