083830a5800801c3ac429f416d9ab9f4bf9256f4
KuMo <thuanhd210203@gmail.com>
2024-07-05 23:46:01 +0700

    Update code setAvatar , and edit user for admin

--- BE/Project_MeBe/src/main/java/com/n3/mebe/service/iml/CloundinaryService.java
+++ BE/Project_MeBe/src/main/java/com/n3/mebe/service/iml/CloundinaryService.java
@@ -43,32 +43,32 @@ public class CloundinaryService implements ICloudinaryService {
         return null;
     }
 
-//    @Scheduled(cron = "0 0 0 * * ?", zone = "Asia/Ho_Chi_Minh") // Chạy vào nửa đêm mỗi ngày theo giờ Hồ Chí Minh
-//    public void cleanupOldImages() {
-//        try {
-//            // Lấy danh sách các hình ảnh
-//            ApiResponse response = cloudinary.api().resources(ObjectUtils.emptyMap());
-//
-//            // Duyệt qua từng hình ảnh
-//            for (Map<String, Object> resource : (Iterable<Map<String, Object>>) response.get("resources")) {
-//                String publicId = (String) resource.get("public_id");
-//                Date createdAt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").parse((String) resource.get("created_at"));
-//
-//                // Kiểm tra nếu hình ảnh đã không được sử dụng trong 3 ngày
-//                // 3 ngày 24 tiếng 60 phút 60 giây
-//                if (new Date().getTime() - createdAt.getTime() > 3 * 24 * 60 * 60 * 1000L) {
-//                    // Xóa hình ảnh
-//                    cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
-//                }
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
+    @Scheduled(cron = "0 0 0 * * ?", zone = "Asia/Ho_Chi_Minh") // Chạy vào nửa đêm mỗi ngày theo giờ Hồ Chí Minh
+    public void cleanupOldImagesAll() {
+        try {
+            // Lấy danh sách các hình ảnh
+            ApiResponse response = cloudinary.api().resources(ObjectUtils.emptyMap());
+
+            // Duyệt qua từng hình ảnh
+            for (Map<String, Object> resource : (Iterable<Map<String, Object>>) response.get("resources")) {
+                String publicId = (String) resource.get("public_id");
+                Date createdAt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").parse((String) resource.get("created_at"));
+
+                // Kiểm tra nếu hình ảnh đã không được sử dụng trong 1 ngày
+                // 3 ngày 24 tiếng 60 phút 60 giây
+                if (new Date().getTime() - createdAt.getTime() > 24 * 60 * 60 * 1000L) {
+                    // Xóa hình ảnh
+                    cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
     @Scheduled(cron = "0 0 0 * * ?", zone = "Asia/Ho_Chi_Minh") // Chạy vào nửa đêm mỗi ngày theo giờ Hồ Chí Minh
-    public void cleanupOldImages() {
+    public void cleanupOldImagesAvatar() {
         String folder = "Avatar_User"; // Thay thế bằng tên thư mục của bạn
         try {
             // Lấy danh sách các hình ảnh trong thư mục cụ thể
