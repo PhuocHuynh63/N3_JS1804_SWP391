89dd953d90f5397e07f7f78f5c5e886d2a590bfd
KuMo <thuanhd210203@gmail.com>
2024-07-06 18:50:57 +0700

    Add code voucher CRUD

---
+++ BE/Project_MeBe/src/main/java/com/n3/mebe/service/IVoucherService.java
@@ -0,0 +1,33 @@
+package com.n3.mebe.service;
+
+
+import com.n3.mebe.dto.request.voucher.VoucherRequest;
+import com.n3.mebe.dto.response.voucher.VoucherResponse;
+import com.n3.mebe.entity.Voucher;
+
+import java.util.List;
+
+public interface IVoucherService {
+
+    Voucher getVoucherById(int id);
+
+    Voucher getVoucherByCode(String code);
+
+    boolean createVoucher(VoucherRequest request);
+
+    boolean updateVoucher(int id , VoucherRequest request);
+
+    boolean setActive(int id, boolean status);
+
+    boolean setPublic(int id, boolean status);
+
+    void deleteVoucher(int id);
+
+    VoucherResponse getVoucherByIdResponse(int id);
+
+    VoucherResponse getVoucherByCodeResponse(String code);
+
+    List<VoucherResponse> searchVoucherByName(String name);
+
+    List<VoucherResponse> getListVoucherResponseAll();
+}
--- BE/Project_MeBe/src/main/java/com/n3/mebe/util/DataUtils.java
+++ BE/Project_MeBe/src/main/java/com/n3/mebe/util/DataUtils.java
@@ -16,4 +16,16 @@ public class DataUtils {
         }
         return optCode;
     }
+
+    public static String generateCode(int length) {
+        String characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        char otp[] = new char[length];
+        Random getOtpNum = new Random();
+        for (int i = 0; i < length; i++) {
+            otp[i] = characters.charAt(getOtpNum.nextInt(characters.length()));
+        }
+        String otpCode = new String(otp);
+        return otpCode;
+    }
+
 }
