e0c20ece04d0ff989cf0d1e2c2f5ff07a0dcdcd8
PhuocHuynh63 <phuochmse171830@fpt.edu.vn>
2024-07-23 09:57:27 +0700

    fix product detail admin

--- FE/FE_MeBeReact/app-project-master/src/pages/adminPage/productPage/viewProductPage/ViewProduct.js
+++ FE/FE_MeBeReact/app-project-master/src/pages/adminPage/productPage/viewProductPage/ViewProduct.js
@@ -53,111 +53,110 @@ export default function ViewProduct() {
 
     return (
         <div className="admin-product-view">
-            <User>
-                <h1>Chi tiết sản phẩm</h1>
-                <form>
-                    <div className="form-row">
-                        <div className="form-group">
-                            <label htmlFor="productName">Tên sản phẩm: <span className="obligatory">*</span></label>
-                            <input
-                                type="text"
-                                id="productName"
-                                value={product.name}
-                                disabled
-                            />
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="subCategory">Loại sản phẩm: <span className="obligatory">*</span></label>
-                            <select
-                                id="subCategory"
-                                value={product.subCategory}
-                                disabled
-                            >
-                                {subCategory.map((item) => (
-                                    <option key={item.subCategoryId} value={item.subCategoryId}>{item.name}</option>
-                                ))}
-                            </select>
-                        </div>
-                    </div>
-
-                    <div className="form-row">
-                        <div className="form-group">
-                            <label htmlFor="price">Giá: <span className="obligatory">*</span></label>
-                            <input
-                                type="number"
-                                id="price"
-                                value={product.price}
-                                disabled
-                            />
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="discount">Giảm giá: <span style={{ color: "red", fontSize: "13px" }}>{product.salePrice.toLocaleString()}đ</span></label>
-                            <input
-                                type="number"
-                                id="discount"
-                                value={discount}
-                                disabled
-                            />
-                        </div>
-                    </div>
-
-                    <div className="form-row">
-                        <div className="form-group">
-                            <label htmlFor="quantity">Số lượng:</label>
-                            <input
-                                type="number"
-                                id="quantity"
-                                value={product.quantity}
-                                disabled
-                            />
-                        </div>
-                    </div>
-
-                    <div className="form-row">
-                        <div className="form-group image">
-                            <label htmlFor="image">Ảnh: <span className="obligatory">*</span></label>
-                            <input type="file" id="image" disabled />
-                        </div>
-                    </div>
-
+            <User />
+            <h1>Chi tiết sản phẩm</h1>
+            <form>
+                <div className="form-row">
                     <div className="form-group">
-                        <label htmlFor="description">Mô tả:</label>
-                        <textarea
-                            id="description"
-                            rows="4"
-                            value={product.description}
+                        <label htmlFor="productName">Tên sản phẩm: <span className="obligatory">*</span></label>
+                        <input
+                            type="text"
+                            id="productName"
+                            value={product.name}
                             disabled
                         />
                     </div>
-                    <div className="form-row">
-                        <div className="form-group">
-                            <label htmlFor="total_sold">Số lượt bán: </label>
-                            <input
-                                type="text"
-                                id="total_sold"
-                                value={product.totalSold}
-                                disabled
-                            />
-                        </div>
 
-                        <div className="form-group">
-                            <label htmlFor="revenue">Doanh thu: </label>
-                            <input
-                                type="text"
-                                id="revenue"
-                                value={calculateRevenue().toLocaleString()}
-                                disabled
-                            />
-                        </div>
+                    <div className="form-group">
+                        <label htmlFor="subCategory">Loại sản phẩm: <span className="obligatory">*</span></label>
+                        <select
+                            id="subCategory"
+                            value={product.subCategory}
+                            disabled
+                        >
+                            {subCategory.map((item) => (
+                                <option key={item.subCategoryId} value={item.subCategoryId}>{item.name}</option>
+                            ))}
+                        </select>
                     </div>
-                    <div className="form-group_btn">
-                        <Link to={'/admin/product'} className="btn-close_add">Quay lại</Link>
-                        <Link to={`/admin/product/update/${productId}`} className="btn-add" style={{ textAlign: "center" }}>Cập nhật <i className="fa-solid fa-arrow-right"></i></Link>
+                </div>
+
+                <div className="form-row">
+                    <div className="form-group">
+                        <label htmlFor="price">Giá: <span className="obligatory">*</span></label>
+                        <input
+                            type="number"
+                            id="price"
+                            value={product.price}
+                            disabled
+                        />
                     </div>
-                </form>
-            </User>
+
+                    <div className="form-group">
+                        <label htmlFor="discount">Giảm giá: <span style={{ color: "red", fontSize: "13px" }}>{product.salePrice.toLocaleString()}đ</span></label>
+                        <input
+                            type="number"
+                            id="discount"
+                            value={discount}
+                            disabled
+                        />
+                    </div>
+                </div>
+
+                <div className="form-row">
+                    <div className="form-group">
+                        <label htmlFor="quantity">Số lượng:</label>
+                        <input
+                            type="number"
+                            id="quantity"
+                            value={product.quantity}
+                            disabled
+                        />
+                    </div>
+                </div>
+
+                <div className="form-row">
+                    <div className="form-group image">
+                        <label htmlFor="image">Ảnh: <span className="obligatory">*</span></label>
+                        <input type="file" id="image" disabled />
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="description">Mô tả:</label>
+                    <textarea
+                        id="description"
+                        rows="4"
+                        value={product.description}
+                        disabled
+                    />
+                </div>
+                <div className="form-row">
+                    <div className="form-group">
+                        <label htmlFor="total_sold">Số lượt bán: </label>
+                        <input
+                            type="text"
+                            id="total_sold"
+                            value={product.totalSold}
+                            disabled
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="revenue">Doanh thu: </label>
+                        <input
+                            type="text"
+                            id="revenue"
+                            value={calculateRevenue().toLocaleString()}
+                            disabled
+                        />
+                    </div>
+                </div>
+                <div className="form-group_btn">
+                    <Link to={'/admin/product'} className="btn-close_add">Quay lại</Link>
+                    <Link to={`/admin/product/update/${productId}`} className="btn-add" style={{ textAlign: "center" }}>Cập nhật <i className="fa-solid fa-arrow-right"></i></Link>
+                </div>
+            </form>
         </div>
     );
 }
